134304097f1ea620531054bd8e2e9d4c7d59fd37
Habib Alizada <habibalizada@gmail.com>
2021-06-26 14:31:01 -0500

    Initial commit

---
+++ e-stockmarket-app/src/main/java/com/estockmarket/estockmarketapp/EStockmarketAppApplication.java
@@ -0,0 +1,13 @@
+package com.estockmarket.estockmarketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EStockmarketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EStockmarketAppApplication.class, args);
+	}
+
+}
