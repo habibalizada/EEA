15a409141862b2fc92ab1f0df3e6f4f20e4aa1bd
Habib Alizada <habibalizada@gmail.com>
2021-07-26 14:19:43 -0500

    @NotEmpty changed to @NotBlank for String properties

--- e-stockmarket-app/src/main/java/com/estockmarket/estockmarketapp/model/Company.java
+++ e-stockmarket-app/src/main/java/com/estockmarket/estockmarketapp/model/Company.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
@@ -22,17 +23,17 @@ public class Company {
     public static final String SEQUENCE_NAME = "companys_sequence";
     @Id
     private long id;
-    @NotEmpty(message = "Code is mandatory")
+    @NotBlank(message = "Code is mandatory")
     private String code;
-    @NotEmpty(message = "Name is mandatory")
+    @NotBlank(message = "Name is mandatory")
     private String name;
-    @NotEmpty(message = "Ceo is mandatory")
+    @NotBlank(message = "Ceo is mandatory")
     private String ceo;
     @NotNull(message = "Turnover is mandatory")
     @DecimalMin(value = "10000000", inclusive = true)
     private BigDecimal turnover;
-    @NotEmpty(message = "Website is mandatory")
+    @NotBlank(message = "Website is mandatory")
     private String website;
-    @NotEmpty(message = "Enlisted stock exchange is mandatory")
+    @NotBlank(message = "Enlisted stock exchange is mandatory")
     private String enlistedStockExchange;
 }
