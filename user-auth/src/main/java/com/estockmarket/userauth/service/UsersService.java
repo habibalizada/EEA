570e8a2c69ba96760c00ef9e1b57f43a3ab4d279
Habib Alizada <habibalizada@gmail.com>
2021-08-19 15:16:13 -0500

    AuthenticationFilter created

--- user-auth/src/main/java/com/estockmarket/userauth/service/UsersService.java
+++ user-auth/src/main/java/com/estockmarket/userauth/service/UsersService.java
@@ -1,7 +1,8 @@
 package com.estockmarket.userauth.service;
 
 import com.estockmarket.userauth.shared.UserDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UsersService {
+public interface UsersService extends UserDetailsService {
     UserDto createUser(UserDto userDetails);
 }
--- user-auth/src/main/java/com/estockmarket/userauth/service/UsersServiceImp.java
+++ user-auth/src/main/java/com/estockmarket/userauth/service/UsersServiceImp.java
@@ -6,9 +6,13 @@ import com.estockmarket.userauth.shared.UserDto;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Service
@@ -35,5 +39,15 @@ public class UsersServiceImp implements UsersService {
         UserDto returnValue = modelMapper.map(userEntity, UserDto.class);
 
         return returnValue;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = usersRepository.findByEmail(username);
+
+        if (userEntity == null){
+            throw new UsernameNotFoundException(username);
+        }
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), true,true,true,true, new ArrayList<>());
     }
 }
