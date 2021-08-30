00c4bd50ebe2c8bf21b0bb77189c9886f569bc7f
Habib Alizada <habibalizada@gmail.com>
2021-08-30 11:37:26 -0500

    user-e-stock-auth microservice added

---
+++ user-e-stock-auth/src/main/java/com/estockmarket/userestockauth/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.estockmarket.userestockauth.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.estockmarket.userestockauth.models.ERole;
+import com.estockmarket.userestockauth.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
+
---
+++ user-e-stock-auth/src/test/java/com/estockmarket/userestockauth/UserEStockAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estockmarket.userestockauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserEStockAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
