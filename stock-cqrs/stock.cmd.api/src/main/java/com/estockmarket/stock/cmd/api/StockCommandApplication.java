c04f50908e52507fed4aa7c101073deb9ec067d7
Habib Alizada <habibalizada@gmail.com>
2021-09-13 14:19:35 -0500

    Axon configuration

--- stock-cqrs/stock.cmd.api/src/main/java/com/estockmarket/stock/cmd/api/StockCommandApplication.java
+++ stock-cqrs/stock.cmd.api/src/main/java/com/estockmarket/stock/cmd/api/StockCommandApplication.java
@@ -1,9 +1,12 @@
 package com.estockmarket.stock.cmd.api;
 
+import com.estockmarket.stock.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({ AxonConfig.class })
 public class StockCommandApplication {
 
 	public static void main(String[] args) {
--- stock-cqrs/stock.query.api/src/main/java/com/estockmarket/stock/query/api/StockQueryApplication.java
+++ stock-cqrs/stock.query.api/src/main/java/com/estockmarket/stock/query/api/StockQueryApplication.java
@@ -1,9 +1,12 @@
 package com.estockmarket.stock.query.api;
 
+import com.estockmarket.stock.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({ AxonConfig.class })
 public class StockQueryApplication {
 
 	public static void main(String[] args) {
